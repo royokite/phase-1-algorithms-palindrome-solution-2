c3285cf6f9f06284e840dd32db5167e4c7722612
RoyOkite <royokite@gmail.com>
2022-09-05 21:52:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // iterate from the beginning of the string to the middle of the string
+  // compare the letter we're iterating over to the corresponding letter at the end of the string
+  // if the letters don't match, return false
+  // if we reach the middle, and all the letters match, return true
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
+
 */
 
 /*
   Add written explanation of your solution here
+  Input: "racecar"
+Input length divided by two: 3.5
+Iteration:
+  Index 0 (less than 3.5, keep iterating)
+  Index 1 (less than 3.5, keep iterating)
+  Index 2 (less than 3.5, keep iterating)
+  Index 3 (less than 3.5, keep iterating)
+  Index 4 (not less than 3.5, stop iterating)
+For "racecar", our loop will iterate up to the middle "e"
+
+Input: "abba"
+Input length divided by two: 2
+Iteration:
+  Index 0 (less than 2, keep iterating)
+  Index 1 (less than 2, keep iterating)
+  Index 2 (not less than 2, stop iterating)
+For "abba", our loop will iterate up to the first "b"
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,6 +50,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
